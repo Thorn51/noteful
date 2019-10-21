91bcad134f79dc1e2d02a4d87687fc4f84a1f6b4
Matt Thornberry <mtberry16@hotmail.com>
2019-10-21 12:55:13 -0600

    Add basic jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./App.css";
 
 export default class App extends React.Component {
   render() {
-    return null;
+    return (
+      <div>
+        <header className="App_header">
+          <h1 className="App_title">Noteful</h1>
+        </header>
+        <main></main>
+      </div>
+    );
   }
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,6 +2,17 @@ import React from "react";
 
 export default class Main extends React.Component {
   render() {
-    return null;
+    return (
+      <div className="main_container">
+        <section className="main_sidebar">
+          <h2>Folder Section</h2>
+          <button className="main_add_folder_button"></button>
+        </section>
+        <section className="main_content">
+          <h2>Notes Section</h2>
+          <button className="main_add_note_button">Add Note</button>
+        </section>
+      </div>
+    );
   }
 }
