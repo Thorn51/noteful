13248b951ce23a04e3c8c3c6bc4c700b1a2847f1
Matt Thornberry <mtberry16@hotmail.com>
2019-10-21 15:58:10 -0600

    Format main component

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -4,14 +4,6 @@ import store from "../store";
 
 export default class Folder extends React.Component {
   render() {
-    const folders = store.folders;
-    const folderList = Object.keys(folders).map(key => {
-      return (
-        <div key={folders[key].id} className="folder">
-          {folders[key].name}
-        </div>
-      );
-    });
-    return <section className="folder_list">{folderList}</section>;
+    return null;
   }
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,18 +1,36 @@
 import React from "react";
 import "./Main.css";
-import Folder from "../Folder/Folder";
+import store from "../store";
 
 export default class Main extends React.Component {
   render() {
+    const folderList = Object.keys(store.folders).map(key => {
+      return (
+        <div key={store.folders[key].id} className="folder">
+          <h2>{store.folders[key].name}</h2>
+        </div>
+      );
+    });
+
+    const noteList = Object.keys(store.notes).map(key => {
+      return (
+        <div key={store.notes[key].id} className="note">
+          <h2>{store.notes[key].name}</h2>
+          {store.notes[key].modified}
+          <button>Delete</button>
+        </div>
+      );
+    });
     return (
       <div className="main_container">
         <section className="main_sidebar">
           <h2>Folder Section</h2>
-          <Folder />
+          {folderList}
           <button className="main_add_folder_button">Add Folder</button>
         </section>
         <section className="main_content">
           <h2>Notes Section</h2>
+          {noteList}
           <button className="main_add_note_button">Add Note</button>
         </section>
       </div>
