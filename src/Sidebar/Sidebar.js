7ee73d84ea19333cdc141d955464bfb057e966a1
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 07:00:24 -0600

    Render folder list

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Sidebar() {
-  return null;
+export default function Sidebar(props) {
+  const folderList = Object.keys(props.folders).map((key, index) => {
+    return (
+      <div key={index}>
+        <h2 className="folder_name">{props.folders[key].name}</h2>
+      </div>
+    );
+  });
+  return <section className="folder_list">{folderList}</section>;
 }
