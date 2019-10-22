0616116966e468d14eb4dcda07374868cc51fa23
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 14:56:27 -0600

    Add folder sidebar jsx

--- src/NoteSidebar/NoteSidebar.js
+++ src/NoteSidebar/NoteSidebar.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Note() {
-  return null;
+export default function Note(props) {
+  const folder = props.folders.find(
+    folder => folder.id === props.match.params.noteId
+  );
+  return (
+    <div className="note_sidebar">
+      <button className="back_button">Go Back</button>
+      <h2 className="folder_name">{folder}</h2>
+    </div>
+  );
 }
