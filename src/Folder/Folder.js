f04d2171b530ce3b10b31a036c40810c46b9b10a
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 12:25:51 -0600

    Add styles

--- src/Folder/Folder.js
+++ src/Folder/Folder.js
@@ -1,4 +1,6 @@
 import React from "react";
+import moment from "moment";
+import "./Folder.css";
 
 export default function Folder(props) {
   const folderNotes = props.notes.filter(
@@ -7,7 +9,14 @@ export default function Folder(props) {
   const notes = Object.keys(folderNotes).map(key => {
     return (
       <div key={folderNotes[key].id} className="note">
-        <h2>{folderNotes[key].name}</h2>
+        <h2 className="note_name">{folderNotes[key].name}</h2>
+        <div className="note_modification">
+          <p className="mod_date">
+            Date modified on {""}
+            {moment(folderNotes[key].modified).format("Do MMM YYYY")}
+          </p>
+          <button className="delete_note">Delete Note</button>
+        </div>
       </div>
     );
   });
