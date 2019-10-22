cb962654b348548c047ab891d9f5e5c82be61df6
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 09:32:56 -0600

    Add NavLink

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 
 export default function Sidebar(props) {
   const folderList = Object.keys(props.folders).map(key => {
     return (
-      <div key={props.folders[key].id} className="folder">
-        <h2 className="folder_name">{props.folders[key].name}</h2>
-      </div>
+      <NavLink
+        to={`/folder/${props.folders[key].id}`}
+        key={props.folders[key].id}
+      >
+        <div className="folder">
+          <h2 className="folder_name">{props.folders[key].name}</h2>
+        </div>
+      </NavLink>
     );
   });
   return <section className="folder_list">{folderList}</section>;
