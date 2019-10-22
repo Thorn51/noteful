d1778540ea34f0134d0e8402193977bf1ce0b5cd
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 06:10:11 -0600

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import "./App.css";
+import Sidebar from "./Sidebar/Sidebar";
 import Main from "./Main/Main";
-import Folder from "./Folder/Folder";
 
 export default class App extends React.Component {
   render() {
@@ -13,9 +13,11 @@ export default class App extends React.Component {
             <h1 className="App_title">Noteful</h1>
           </Link>
         </header>
+        <aside className="sidebar">
+          <Route exact path="/" component={Sidebar} />
+        </aside>
         <main>
           <Route exact path="/" component={Main} />
-          <Route path="/folder" component={Folder} />
         </main>
       </div>
     );
