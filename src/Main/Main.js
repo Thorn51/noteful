04da867c1caf2072db65e5750804ecb3c460bfd3
Matt Thornberry <mtberry16@hotmail.com>
2019-10-22 07:01:28 -0600

    Render notes list

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Main() {
-  return null;
+export default function Main(props) {
+  const noteList = Object.keys(props.notes).map(key => {
+    return (
+      <div key={props.notes[key].id} className="note">
+        <h2 className="note_name">{props.notes[key].name}</h2>
+        <div>
+          <p className="mod_date">{props.notes[key].modified}</p>
+          <button className="delete_note">Delete Note</button>
+        </div>
+      </div>
+    );
+  });
+  return <div className="note_list">{noteList}</div>;
 }
