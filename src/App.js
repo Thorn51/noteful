ebb5e85725fcaaaae7c6b7066ebe050f8d27dbad
Matt Thornberry <mtberry16@hotmail.com>
2019-10-21 20:46:56 -0600

    Add links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Main from "./Main/Main";
+import Folder from "./Folder/Folder";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <header className="App_header">
-          <h1 className="App_title">Noteful</h1>
+          <Link to="/">
+            <h1 className="App_title">Noteful</h1>
+          </Link>
         </header>
         <main>
-          <Main />
+          <Route exact path="/" component={Main} />
+          <Route path="/folder" component={Folder} />
         </main>
       </div>
     );
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Main.css";
 import store from "../store";
 import moment from "moment";
@@ -7,9 +8,11 @@ export default class Main extends React.Component {
   render() {
     const folderList = Object.keys(store.folders).map(key => {
       return (
-        <div key={store.folders[key].id} className="folder">
-          <h2>{store.folders[key].name}</h2>
-        </div>
+        <NavLink to="/folder" key={store.folders[key].id}>
+          <div className="folder">
+            <h2>{store.folders[key].name}</h2>
+          </div>
+        </NavLink>
       );
     });
 
@@ -30,12 +33,12 @@ export default class Main extends React.Component {
     return (
       <div className="main_container">
         <section className="main_sidebar">
-          <h2>Folder Section</h2>
+          <h2>Folders</h2>
           {folderList}
           <button className="main_add_folder_button">Add Folder</button>
         </section>
         <section className="main_content">
-          <h2>Notes Section</h2>
+          <h2>Notes</h2>
           {noteList}
           <button className="main_add_note_button">Add Note</button>
         </section>
