9918e43be6af1cca272f6688a052cc196bc2a3df
Matt Thornberry <mtberry16@hotmail.com>
2019-10-23 08:22:35 -0600

    Implement go back feature on NoteSidebar

--- src/NoteSidebar/NoteSidebar.js
+++ src/NoteSidebar/NoteSidebar.js
@@ -5,12 +5,16 @@ export default function Note(props) {
   const note = props.notes.filter(
     note => note.id === props.match.params.noteId
   );
-  console.log(props.history);
   const folder = props.folders.filter(folder => folder.id === note[0].folderId);
   return (
     <aside className="folder_sidebar">
       <div className="note_sidebar">
-        <button className="back_button">Go Back</button>
+        <button
+          onClick={({ history }) => props.history.goBack()}
+          className="back_button"
+        >
+          Go Back
+        </button>
         <div className="note_location">
           <h2 className="folder_name">{folder[0].name}</h2>
         </div>
