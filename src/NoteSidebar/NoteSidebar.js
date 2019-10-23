3fa5b82160f5719a0ee74e91a9d04b0e94edf9f0
Matt Thornberry <mtberry16@hotmail.com>
2019-10-23 06:01:08 -0600

    Tweak styles

--- src/NoteSidebar/NoteSidebar.js
+++ src/NoteSidebar/NoteSidebar.js
@@ -7,13 +7,10 @@ export default function Note(props) {
   );
   console.log(props.history);
   const folder = props.folders.filter(folder => folder.id === note[0].folderId);
-  console.log(note, folder);
   return (
     <aside className="folder_sidebar">
       <div className="note_sidebar">
-        <div className="back_button">
-          <h2>Go Back</h2>
-        </div>
+        <button className="back_button">Go Back</button>
         <div className="note_location">
           <h2 className="folder_name">{folder[0].name}</h2>
         </div>
