91abfe44f4dd3be183348163802773a343e070e6
Matt Thornberry <mtberry16@hotmail.com>
2019-11-01 09:18:47 -0600

    Add snapshot

--- src/AddFolder/AddFolder.test.js
+++ src/AddFolder/AddFolder.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AddFolder from "./AddFolder";
+import renderer from "react-test-renderer";
 
 describe("AddFolder", () => {
   it("renders without throwing errors", () => {
     const div = document.createElement("div");
     ReactDOM.render(<AddFolder />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+  it("renders the component as expected", () => {
+    const tree = renderer.create(<AddFolder />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
